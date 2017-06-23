e80cb2ce453fd0d3708d849bbffb192bed32d3d0
Carlos Arriaga <carlosarriagacm@gmail.com>
2017-06-23 15:19:08 -0500

    fix upstream

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -3752,7 +3752,6 @@ static long perf_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 	return ret;
 }
 
-
 #ifdef CONFIG_COMPAT
 static long perf_compat_ioctl(struct file *file, unsigned int cmd,
 				unsigned long arg)
@@ -3768,24 +3767,6 @@ static long perf_compat_ioctl(struct file *file, unsigned int cmd,
 	}
 	return perf_ioctl(file, cmd, arg);
 }
-<<<<<<< HEAD
-=======
-#ifdef CONFIG_COMPAT
-static long perf_compat_ioctl(struct file *file, unsigned int cmd,
-				unsigned long arg)
-{
-	switch (_IOC_NR(cmd)) {
-	case _IOC_NR(PERF_EVENT_IOC_SET_FILTER):
-		/* Fix up pointer size (usually 4 -> 8 in 32-on-64-bit case */
-		if (_IOC_SIZE(cmd) == sizeof(compat_uptr_t)) {
-			cmd &= ~IOCSIZE_MASK;
-			cmd |= sizeof(void *) << IOCSIZE_SHIFT;
-		}
-		break;
-	}
-	return perf_ioctl(file, cmd, arg);
-}
->>>>>>> 8f97bec... Update the Linux Base Version (3.10.49--->3.10.104)
 #else
 # define perf_compat_ioctl NULL
 #endif
